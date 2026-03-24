350fb4d7ccfc95d74a704095028581e81e607849
Roman <rkizhaev@yandex.ru>
2026-03-24 13:44:32 +0300

    gitignore fix, code refactoring

--- backend/app/fixtures/batch_50_mock.py
+++ backend/app/fixtures/batch_50_mock.py
@@ -9,7 +9,6 @@ def create_batch_file(filename="batch_50.json", count=50):
 
     for i in range(count):
         reading = {
-            # создайте датчик с таким же uuid
             "sensor_id": 'a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11',
             "time": (now - timedelta(minutes=i*10)).isoformat(),
             "value": round(random.uniform(18.0, 26.0), 2)
